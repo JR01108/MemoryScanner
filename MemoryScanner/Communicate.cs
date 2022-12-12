0894f8c1d268e7a38116e9e3bb0f714e23a1b06b
Даниил <dronow220701@gmaiil.com>
2022-12-12 15:45:10 +0300

    Переделана передача файлов пользовательскому интерфейсу

--- MemoryScanner/Communicate.cs
+++ MemoryScanner/Communicate.cs
@@ -8,5 +8,6 @@ namespace MemoryScanner
 {
     internal class Communicate
     {
+
     }
 }
--- MemoryScanner/Logic.cs
+++ MemoryScanner/Logic.cs
@@ -5,19 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Linq.Expressions;
 
 namespace MemoryScanner
 {
     class Logic
     {
-        // Делает лист с данными про папку, ее подпапки и файлы
-        public static void ListOfSubfolder(string path)
+        public static FileTree ListOfSubfolder(string path)
         {
             DirectoryInfo dir = new DirectoryInfo(path);
+            Node startDireectory = new Node(path, SumFolder(path), true);
+            FileTree fileTree = new FileTree(startDireectory);
             DirectoryInfo[] dirs = dir.GetDirectories();
             FileInfo[] files = dir.GetFiles();
-            Node[] nodes = new Node[dirs.Length];
-            Node[] nodesFiles = new Node[files.Length];
+            while(dir.)
             for (int i = 0; i < dirs.Length; i++)
             {
                 nodes[i] = new Node(dirs[i].Name, SumFolder(dirs[i].FullName), IsCatalogOrFile(dirs[i].FullName));
@@ -29,7 +30,6 @@ namespace MemoryScanner
             var result = nodes.Union(nodesFiles);
             List<Node> Children = new List<Node>();
             Children.AddRange(result);
-
         }
         // Считает размер папки, подпапок и файлов
         public static double SumFolder(string path)
@@ -58,14 +58,7 @@ namespace MemoryScanner
         public static bool IsCatalogOrFile(string path)
         {
             FileAttributes attr = File.GetAttributes(path);
-            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return ((attr & FileAttributes.Directory) == FileAttributes.Directory);
         }
     }
 }
